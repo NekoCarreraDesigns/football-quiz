38e1f29b3c5194246e38f85236b35be84c04cfda
NekoCarreraDesigns <denvermaas@gmail.com>
2021-09-29 13:20:39 -0600

    added functions for the app

--- index.js
+++ index.js
@@ -44,3 +44,111 @@ var penalty = 15;
 var incorrect = 0;
 // possible options for each question corresponding to each button id
 var options = ["a", "b", "c", "d"];
+
+// function to start the quiz
+function startQuiz() {
+  init();
+  console.log("start button clicked");
+  quizStartEl.classList.add("hidden");
+  quizQuestionsEl.classList.remove("hidden");
+  displayQuestions();
+  startTimer();
+}
+//  function to reset the time and score
+function init() {
+  index = 0;
+  timeLeft = 75;
+  score = 0;
+  finalScore = 0;
+  penaltyCount = 0;
+  quizStartEl.classList.remove("hidden");
+  quizQuestionsEl.classList.add("hidden");
+}
+// function for starting the countdown timer
+function startTimer() {
+  countdownEl.textContent = timeLeft;
+  timerInterval = setInterval(function () {
+    timeLeft--;
+    if (timeLeft === 0) {
+      gameOver();
+    }
+  }, 1000);
+}
+// display questions on the page
+function displayQuestions() {
+  if (index < myQuestions.length) {
+    questionText.textContent = myQuestions[index].question;
+    displayChoices();
+  } else {
+    gameOver();
+  }
+}
+// populate the answer buttons with question choices
+function displayChoices() {
+  for (j = 0; j < options.length; j++) {
+    answerBtns[j].textContent = myQuestions[index].choices[options[j]];
+  }
+}
+// compare the user answer to the correct answer
+function checkAnswer() {
+  correctAnswer = myQuestions[index].answer;
+  userAnswer = this.id;
+  if (userAnswer === correctAnswer) {
+    answerCorrect();
+  } else {
+    answerIncorrect();
+  }
+  index++;
+  displayQuestions();
+}
+// displays a success message and increases score
+function answerCorrect() {
+  feedbackText.setAttribute("style", "color: green");
+  feedbackText.textContent = "Touchdown!";
+  score = score + correct;
+}
+// displays a failure message and decreases the time
+function answerIncorrect() {
+  feedbackText.setAttribute("style", "color: red");
+  feedbackText.textContent = "You were sacked on the play";
+  score = score - incorrect;
+  timeLeft = timeLeft - penalty;
+}
+//  function for the final answer or time runs out
+function gameOver() {
+  clearInterval(timerInterval);
+  quizQuestionsEl.classList.add("hidden");
+  quizCompleteEl.classList.remove("hidden");
+  calculateFinalScore();
+  finalScoreEl.textContent = finalScore;
+}
+// function for calculating the score
+function calculateFinalScore() {
+  finalScore = score;
+}
+// function to save the score to local storage
+function saveScore() {
+  var initials = userInitialsInput.value;
+
+  var userScoreObject = {
+    initials: initials,
+    score: finalScore,
+  };
+  var allHighScores = localStorage.getItem("storedScoreKey");
+
+  if (allHighScores == null) {
+    localStorage.setItem("storedScoreKey", JSON.stringify([userScoreObject]));
+  } else {
+    storedScoreKey = JSON.parse(allHighScores);
+    storedScoreKey.push(userScoreObject);
+    localStorage.setItem("storedScoreKey", JSON.stringify(storedScoreKey));
+  }
+  window.location.href = "highscores.html";
+}
+// event listeners for buttons
+startBtn.addEventListener("click", startQuiz);
+submitScoreBtn.addEventListener("click", saveScore);
+// any time an answer button is chosen
+for (var answerBtn of answerBtns) {
+  answerBtn.addEventListener("click", checkAnswer);
+}
