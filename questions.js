713d6a97e692aa8c6af770799e3ba843575719f5
NekoCarreraDesigns <denvermaas@gmail.com>
2021-09-29 14:00:32 -0600

    added questions to questions.js

---
+++ questions.js
@@ -0,0 +1,63 @@
+var myQuestions = [
+  {
+    question: "Who was the GOAT before Tom Brady?",
+    choices: {
+      a: "Joe Montana",
+      b: "Dan Marino",
+      c: "Peyton Manning",
+      d: "Aaron Rodgers",
+    },
+    answer: "a",
+  },
+  {
+    question: "Who is the greatest Receiver of  all time?",
+    choices: {
+      a: "Devontae Adams",
+      b: "Calvin Johnson",
+      c: "Randy Moss",
+      d: "Jerry Rice",
+    },
+    answer: "d",
+  },
+  {
+    question: "Who is the greatest running back of all time?",
+    choices: {
+      a: "Ahman Green",
+      b: "Ladanian Tomlinson",
+      c: "Barry Sanders",
+      d: "Emmitt Smith",
+    },
+    answer: "d",
+  },
+  {
+    question: "which team is the oldest in the nfl?",
+    choices: {
+      a: "Chicago Bears",
+      b: "Green Bay Packers",
+      c: "Kansas City Chiefs",
+      d: "Cleveland Browns",
+    },
+    answer: "b",
+  },
+  {
+    question: "Which stadium is the oldest in the nfl",
+    choices: {
+      a: "Arrowhead Stadium",
+      b: "Ford Field",
+      c: "Lambeau Field",
+      d: "Soldier Field",
+    },
+    answer: "c",
+  },
+  {
+    question:
+      "This defensive player was known as the minister of defense, who is he?",
+    choices: {
+      a: "Bruce Smith",
+      b: "Reggie White",
+      c: "Von Miller",
+      d: "Aaron Donald",
+    },
+    answer: "b",
+  },
+];
